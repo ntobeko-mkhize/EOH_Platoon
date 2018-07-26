25132f20c35cf912cf88bd7fd9168dc5ea9c90d5
ntobekoMkhize <ntobekoMkhize@users.noreply.github.com>
2018-07-26 10:21:31 +0200

    initial commit

---
+++ src/main/java/za/co/mkhizenj/digitalpservice/DigitalPServiceApplication.java
@@ -0,0 +1,12 @@
+package za.co.mkhizenj.digitalpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalPServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalPServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/za/co/mkhizenj/digitalpservice/DigitalPServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package za.co.mkhizenj.digitalpservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DigitalPServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
